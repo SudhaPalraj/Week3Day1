596f24a1fd5f614b8d234b1cc9c1486a5a66eaf2
SudhaPalraj <93433360+SudhaPalraj@users.noreply.github.com>
2021-11-13 23:17:31 +0530

    Add files via upload

---
+++ IndexofString.java
@@ -0,0 +1,25 @@
+package week3.day1;
+
+public class IndexofString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String text = "Java Exercise ";
+
+		int indexOf = text.indexOf('E');
+
+		System.out.println(indexOf);
+		int length = text.length();
+		System.out.println(length);
+		char charAt = text.charAt(5);
+
+		System.out.println(charAt);
+
+		char charAt1 = text.charAt(11);
+
+		System.out.println(charAt1);
+
+	}
+
+}
