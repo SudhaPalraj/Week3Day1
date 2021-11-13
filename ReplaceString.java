467addf579ac123bd4010d14794a63c7ea1f5840
SudhaPalraj <93433360+SudhaPalraj@users.noreply.github.com>
2021-11-13 23:30:05 +0530

    Add files via upload

---
+++ ReplaceString.java
@@ -0,0 +1,21 @@
+package week3.day1;
+
+public class ReplaceString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String sentence = "I am working with Java8";
+		
+		String replacedString = sentence.replace("Java8", "Java11");
+		
+		System.out.println(replacedString);
+		
+		
+		
+		
+		
+		
+	}
+
+}
